b4b176f6670b9e359d0c3cc1b61a53f64457013e
mouhcine khairat <ton.email@example.com>
2025-11-02 16:07:48 +0100

    modification du path du Hello-world à Hello

--- src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/HelloResource.java
+++ src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/HelloResource.java
@@ -5,7 +5,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
-@Path("/hello-world")
+@Path("/hello")
 public class HelloResource {
     @GET
     @Produces("text/plain")

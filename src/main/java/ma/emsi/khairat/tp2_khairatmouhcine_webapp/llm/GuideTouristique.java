14fe85ff79de7e9cc78001fa5561029216c82b53
mouhcine khairat <ton.email@example.com>
2025-11-02 16:58:57 +0100

    Ajout des dépendances LangChain4j et création des classes GuideTouristique et LlmClientForGuide

---
+++ src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/llm/GuideTouristique.java
@@ -0,0 +1,22 @@
+package ma.emsi.khairat.tp2_khairatmouhcine_webapp.llm;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+
+
+public interface GuideTouristique {
+
+    @SystemMessage("""
+        Vous êtes un guide touristique.
+        Lorsqu'on vous donne un nom de pays ou de ville, 
+        vous indiquez les 2 principaux endroits à visiter 
+        et le prix moyen d’un repas dans la devise locale.
+        Répondez au format JSON suivant :
+        {
+          "ville_ou_pays": "nom du lieu",
+          "endroits_a_visiter": ["endroit1", "endroit2"],
+          "prix_moyen_repas": "<prix> <devise>"
+        }
+    """)
+    String getInfos(@UserMessage String lieu);
+}
---
+++ src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/llm/LlmClientForGuide.java
@@ -0,0 +1,31 @@
+package ma.emsi.khairat.tp2_khairatmouhcine_webapp.llm;
+
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class LlmClientForGuide {
+
+    private final GuideTouristique assistant;
+
+    public LlmClientForGuide() {
+        String apiKey = System.getenv("GEMINI_KEY");
+
+        ChatModel model = GoogleAiGeminiChatModel.builder()
+                .apiKey(apiKey)
+                .modelName("gemini-2.5-flash")
+                .build();
+
+        this.assistant = AiServices.builder(GuideTouristique.class)
+                .chatModel(model)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                .build();
+    }
+
+    public String getInfos(String lieu) {
+        return assistant.getInfos(lieu);
+    }
+}

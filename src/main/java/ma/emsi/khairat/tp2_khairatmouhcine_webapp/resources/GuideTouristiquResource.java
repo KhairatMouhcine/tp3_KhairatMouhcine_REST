d53fac8069f1d64630d451b609ed2e5ce7e736c1
mouhcine khairat <ton.email@example.com>
2025-11-02 16:21:39 +0100

    Création de la ressource REST GuideTouristiquResource avec @Path("/guide")  et  la méthode getInfos annotée @GET, @Path et @Produces (GET /api/guide/lieu/{ville_ou_pays}) retournant le lieu en texte brut

--- src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/resources/GuideTouristiquResource.java
+++ src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/resources/GuideTouristiquResource.java
@@ -1,4 +1,16 @@
 package ma.emsi.khairat.tp2_khairatmouhcine_webapp.resources;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+
+@Path("/guide")
 public class GuideTouristiquResource {
+    @GET
+    @Path("lieu/{ville_ou_pays}")
+    @Produces("text/plain")
+    public String getInfos(@PathParam("ville_ou_pays") String lieu) {
+        return lieu;
+    }
 }

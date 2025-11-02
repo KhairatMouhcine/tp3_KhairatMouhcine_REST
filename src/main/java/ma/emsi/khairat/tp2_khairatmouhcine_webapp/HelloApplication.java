b8db341828c5d46290b248e3c56e0db760d7db14
mouhcine khairat <ton.email@example.com>
2025-11-02 15:54:50 +0100

    Creation du projet Jakarta EE 10 avec Rest

---
+++ src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.khairat.tp2_khairatmouhcine_webapp;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}

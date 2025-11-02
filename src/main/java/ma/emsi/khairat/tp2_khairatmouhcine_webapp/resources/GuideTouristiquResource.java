b8e489a97a4fcfab7c1a44bb9d969fbae268e6b2
mouhcine khairat <ton.email@example.com>
2025-11-02 16:25:32 +0100

    Modification du la methode getInfos pour qu'il renvoie un tableau au lieu de text brute

--- src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/resources/GuideTouristiquResource.java
+++ src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/resources/GuideTouristiquResource.java
@@ -4,13 +4,15 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/guide")
 public class GuideTouristiquResource {
     @GET
     @Path("lieu/{ville_ou_pays}")
-    @Produces("text/plain")
-    public String getInfos(@PathParam("ville_ou_pays") String lieu) {
-        return lieu;
+    @Produces(MediaType.APPLICATION_JSON)
+    public String[]  getInfos(@PathParam("ville_ou_pays") String lieu) {
+        // Retourne un tableau contenant un seul élément : le nom du lieu
+        return new String[]{lieu};
     }
 }

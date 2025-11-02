6672e8362635c4bc6098abcf8e2dde8c3e087bff
mouhcine khairat <ton.email@example.com>
2025-11-02 16:05:44 +0100

    Ajout du paramètre nom dans HelloResource (GET /api/hello-world/personnes/{nom})

--- src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/HelloResource.java
+++ src/main/java/ma/emsi/khairat/tp2_khairatmouhcine_webapp/HelloResource.java
@@ -2,13 +2,15 @@ package ma.emsi.khairat.tp2_khairatmouhcine_webapp;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Path("personnes/{nom}") // <-- partie dynamique dans l'URL
+    public String hello(@PathParam("nom") String nom) {
+        return "Hello, " + nom + "!";
     }
 }
